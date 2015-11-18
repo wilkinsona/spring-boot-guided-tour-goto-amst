7679f36abeed1abff1abad0d07dfad8d00ca2964
Andy Wilkinson <awilkinson@pivotal.io>
2015-11-18 15:29:54 +0000

    Enable global method security and require HERO role to access /

--- demo/src/main/java/com/example/DemoApplication.java
+++ demo/src/main/java/com/example/DemoApplication.java
@@ -2,8 +2,10 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class DemoApplication {
 
 	public static void main(String[] args) {
--- demo/src/main/java/com/example/HomeController.java
+++ demo/src/main/java/com/example/HomeController.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 
 	@RequestMapping("/")
+	@Secured("ROLE_GOTO")
 	public String home() {
 		return "Hello, Amsterdam";
 	}
