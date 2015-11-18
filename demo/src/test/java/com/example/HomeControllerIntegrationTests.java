fc02599247dcc27c648e49420f5f8bf9921778de
Andy Wilkinson <awilkinson@pivotal.io>
2015-11-18 14:21:54 +0000

    Add an integration test for HomeController

---
+++ demo/src/test/java/com/example/HomeControllerIntegrationTests.java
@@ -0,0 +1,29 @@
+package com.example;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.client.RestTemplate;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(DemoApplication.class)
+@WebIntegrationTest(randomPort = true)
+public class HomeControllerIntegrationTests {
+
+	@Value("${local.server.port}")
+	private int port;
+
+	@Test
+	public void home() {
+		String body = new RestTemplate().getForObject("http://localhost:" + port, String.class);
+		assertThat(body,  is(equalTo("Hello, Amsterdam")));
+	}
+
+}
